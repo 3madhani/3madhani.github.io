5ab4e28c545687aefdb67dd892c34eeded60bd17
3mad Hany <144627820+3madhani@users.noreply.github.com>
2025-10-12 03:02:37 +0300

    Update and deploy 2025-10-12 03:02:37

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1e9a811bf8e70466596bcf0ea3a8b5adb5f17f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3606265300"
+    serviceWorkerVersion: "1513198928"
   }
 });
